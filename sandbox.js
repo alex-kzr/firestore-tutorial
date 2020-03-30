321722ace476cb733bccbfc115c82d317c7dad1f
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-30 17:20:02 +1000

    Add getting data from collections

--- sandbox.js
+++ sandbox.js
@@ -2,3 +2,23 @@
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+
+const list = document.querySelector('ul');
+
+const addRecipe = recipe => {
+    let html = `
+        <li>
+            <div>${recipe.title}</div>
+            <div>${recipe.created_at.toDate()}</div>
+        </li>
+    `;
+    list.innerHTML += html;
+};
+
+db.collection('recipes').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        addRecipe(doc.data());
+    }); 
+}).catch(err => {
+    console.log(err);
+});
