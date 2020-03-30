f9545d153066dabb787da8b93e51dc85c324c4ee
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-30 17:41:12 +1000

    Add saving data to a collection

--- sandbox.js
+++ sandbox.js
@@ -4,6 +4,7 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 const list = document.querySelector('ul');
+const form = document.querySelector('form');
 
 const addRecipe = recipe => {
     let html = `
@@ -15,10 +16,28 @@ const addRecipe = recipe => {
     list.innerHTML += html;
 };
 
+// get documents
 db.collection('recipes').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         addRecipe(doc.data());
     }); 
 }).catch(err => {
     console.log(err);
+});
+
+// add documents
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    const now = new Date();
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    }
+
+    db.collection('recipes').add(recipe).then(() => {
+        console.log('recipe added');
+    }).catch(err => {
+        console.log(err);
+    });
 });
